ba50ed7cf384e47b3b3381c2ef08ae3ef30485d8
ak4422920 <ak4422920@gmail.com>
2025-05-06 15:27:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/AkMoviesHubBackup">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/cineoriginals>[Cineoriginals]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/AkMoviesHubBackup">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/cineoriginals>[@Cineoriginals]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
